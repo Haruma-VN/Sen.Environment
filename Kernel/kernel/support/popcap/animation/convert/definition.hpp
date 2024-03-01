f9c5eb0be8cfa2996373dcfd291607816503b941
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2024-03-01 22:46:28 +0700

    03.01.2024
    Update Kernel

--- Kernel/kernel/support/popcap/animation/convert/definition.hpp
+++ Kernel/kernel/support/popcap/animation/convert/definition.hpp
@@ -2,38 +2,140 @@
 
 #include "kernel/support/popcap/animation/definition.hpp"
 
-namespace Sen::Kernel::Support::PopCap::Animation::Convert {
+namespace Sen::Kernel::Support::PopCap::Animation::Convert
+{
 
-	struct ExtraInfo {
+	struct ExtraInfo
+	{
 		int version;
 		nlohmann::ordered_json group;
 	};
 
 	inline auto static to_json(
-		nlohmann::ordered_json& nlohmann_json_j, 
-		const ExtraInfo& nlohmann_json_t
-	) -> void
+		nlohmann::ordered_json &nlohmann_json_j,
+		const ExtraInfo &nlohmann_json_t) -> void
 	{
-		nlohmann_json_j["version"] = nlohmann_json_t.version; 
+		nlohmann_json_j["version"] = nlohmann_json_t.version;
 		nlohmann_json_j["group"] = nlohmann_json_t.group;
 		return;
-	} 
-	
+	}
+
 	inline auto static from_json(
-		const nlohmann::ordered_json& nlohmann_json_j, 
-		ExtraInfo& nlohmann_json_t
-	) -> void
+		const nlohmann::ordered_json &nlohmann_json_j,
+		ExtraInfo &nlohmann_json_t) -> void
 	{
-		nlohmann_json_j.at("version").get_to(nlohmann_json_t.version); 
+		nlohmann_json_j.at("version").get_to(nlohmann_json_t.version);
 		nlohmann_json_j.at("group").get_to(nlohmann_json_t.group);
 		return;
 	};
 
-	struct Common {
+	struct Common
+	{
 
-		inline static auto constexpr initial_transform = std::array<float, 6> { 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f };
+		inline static auto constexpr initial_transform = std::array<double, 6>{1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f};
 
-		inline static auto constexpr initial_color = std::array<float, 4>{1.0f, 1.0f, 1.0f, 1.0f};
+		inline static auto constexpr initial_color = std::array<double, 4>{1.0f, 1.0f, 1.0f, 1.0f};
+	};
 
+	struct Model
+	{
+	public:
+		int index;
+		int8_t state;
+		int resource;
+		bool sprite;
+		int frame_start;
+		int frame_duration;
+		std::array<double, 6> transform{1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f};
+		std::array<double, 4> color{1.0f, 1.0f, 1.0f, 1.0f};
+
+		explicit Model(
+
+			) = default;
+
+		explicit constexpr Model(
+			int index,
+			int8_t state,
+			int resource,
+			bool sprite,
+			int frame_start,
+			int frame_duration) : index(index), state(state), resource(resource), sprite(sprite),
+								  frame_start(frame_start), frame_duration(frame_duration)
+		{
+		}
+
+		~Model(
+
+			) = default;
+	};
+
+	struct FrameNode
+	{
+	public:
+		int index;
+		int duration;
+		int resource;
+		bool sprite;
+		int first_frame;
+		std::array<double, 6> transform{1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f};
+		std::array<double, 4> color{1.0f, 1.0f, 1.0f, 1.0f};
+
+		explicit FrameNode(
+
+			) = default;
+
+		explicit FrameNode(
+			int index,
+			int duration,
+			int resource,
+			bool sprite,
+			int first_frame
+			) : index(index), duration(duration), resource(resource), sprite(sprite),
+													first_frame(first_frame)
+		{
+		}
+
+		~FrameNode(
+
+			) = default;
+	};
+
+	struct ActionFrameNode
+	{
+	public:
+		int index;
+		int duration;
+		bool use_transform;
+		std::vector<double> transform;
+		bool use_color;
+		std::array<double, 4> color;
+	};
+
+	struct Action
+	{
+	public:
+		int start_index;
+		int duration;
+		std::vector<int> frame_index;
+
+		explicit Action(
+
+			) = default;
+
+		explicit constexpr Action(
+			int start_index,
+			int duration) : start_index(start_index), duration(duration)
+		{
+		}
+
+		~Action(
+
+			) = default;
+	};
+
+	struct Instance
+	{
+		int resource;
+		bool sprite;
 	};
 }
--- Kernel/kernel/support/popcap/animation/convert/to_flash.hpp
+++ Kernel/kernel/support/popcap/animation/convert/to_flash.hpp
@@ -3,146 +3,287 @@
 #include "kernel/support/popcap/animation/definition.hpp"
 #include "kernel/support/popcap/animation/convert/definition.hpp"
 
-namespace Sen::Kernel::Support::PopCap::Animation::Convert {
+namespace Sen::Kernel::Support::PopCap::Animation::Convert
+{
 
-	class ToFlash : public Common {
+	class ToFlash : public Common
+	{
 
-		private:
+	private:
+		using XMLDocument = tinyxml2::XMLDocument;
 
-			using XMLDocument = tinyxml2::XMLDocument;
+		using XMLElement = tinyxml2::XMLElement;
 
-			using XMLElement = tinyxml2::XMLElement;
+		using ExtraInfo = ExtraInfo;
 
-			using ExtraInfo = ExtraInfo;
+		using Common = Common;
 
-			using Common = Common;
+		using Animation = SexyAnimation;
 
-			using Animation = SexyAnimation;
+	protected:
+		template <auto point, typename T>
+			requires std::is_integral<T>::value or std::is_floating_point<T>::value
+		inline static auto to_fixed(
+			T number) -> std::string
+		{
+			auto stream = std::ostringstream{};
+			stream << std::fixed << std::setprecision(point) << number;
+			return stream.str();
+		}
 
+		template <typename T>
+			requires std::is_integral<T>::value or std::is_floating_point<T>::value
+		inline static auto constexpr to_fixed_6 = to_fixed<6, T>;
 
-		protected:
-			
-			template <auto point, typename T> requires std::is_integral<T>::value or std::is_floating_point<T>::value
-			inline static auto to_fixed(
-				T number
-			) -> std::string
+		inline static auto variant_to_standard(
+			std::array<double, 6> &transform,
+			const std::vector<double> &variant_transform) -> void
+		{
+			switch (variant_transform.size())
 			{
-				auto stream = std::ostringstream{};
-				stream << std::fixed << std::setprecision(point) << number;
-				return stream.str();
-			}
-
-			template <typename T> requires std::is_integral<T>::value or std::is_floating_point<T>::value
-			inline static auto constexpr to_fixed_6 = to_fixed<6, T>;
-
-		protected:
-
-			Animation animation;
-
-		protected:
-
-			template <typename T> requires std::is_integral<T>::value or std::is_floating_point<T>::value
-			auto write_image(
-				XMLDocument* document,
-				std::string_view name,
-				nlohmann::ordered_json& image,
-				T scale
-			) -> void
+			case 2:
 			{
-				assert_conditional(image["transform"].get<std::vector<float>>().size() == 6, "transform size must be 6 to write image", "write_image");
-				auto DOMSymbolItem = document->NewElement("DOMSymbolItem");
-				DOMSymbolItem->SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-				DOMSymbolItem->SetAttribute("xmlns", "http://ns.adobe.com/xfl/2008/");
-				DOMSymbolItem->SetAttribute("name", fmt::format("image/{}", name).data());
-				DOMSymbolItem->SetAttribute("symbolType", "graphic");
-				auto timeline = document->NewElement("timeline");
-				auto DOMTimeline = document->NewElement("DOMTimeline");
-				DOMTimeline->SetAttribute("name", name.data());
-				auto layers = document->NewElement("layers");
-				auto DOMLayer = document->NewElement("DOMLayer");
-				auto frames = document->NewElement("frames");
-				auto DOMFrame = document->NewElement("DOMFrame");
-				DOMFrame->SetAttribute("index", 0);
-				auto elements = document->NewElement("elements");
-				auto DOMBitmapInstance = document->NewElement("DOMBitmapInstance");
-				DOMBitmapInstance->SetAttribute("libraryItemName", fmt::format("media/{}", image["name"].get<std::string>()).data());
-				auto matrix = document->NewElement("matrix");
-				auto Matrix = document->NewElement("Matrix");
-				Matrix->SetAttribute("a", to_fixed_6<double>(image["transform"][0].get<double>() * scale).data());
-				Matrix->SetAttribute("b", to_fixed_6<double>(image["transform"][1].get<double>()).data());
-				Matrix->SetAttribute("c", to_fixed_6<double>(image["transform"][2].get<double>()).data());
-				Matrix->SetAttribute("d", to_fixed_6<double>(image["transform"][3].get<double>() * scale).data());
-				Matrix->SetAttribute("tx", to_fixed_6<double>(image["transform"][4].get<double>()).data());
-				Matrix->SetAttribute("ty", to_fixed_6<double>(image["transform"][5].get<double>()).data());
-				matrix->InsertEndChild(Matrix);
-				DOMBitmapInstance->InsertEndChild(matrix);
-				elements->InsertEndChild(DOMBitmapInstance);
-				DOMFrame->InsertEndChild(elements);
-				frames->InsertEndChild(DOMFrame);
-				DOMLayer->InsertEndChild(frames);
-				layers->InsertEndChild(DOMLayer);
-				DOMTimeline->InsertEndChild(layers);
-				timeline->InsertEndChild(DOMTimeline);
-				DOMSymbolItem->InsertEndChild(timeline);
-				document->InsertEndChild(DOMSymbolItem);
-				return;
+				transform = {1.0f, 0.0f, 0.0f, 0.0f, variant_transform[0], variant_transform[1]};
+				break;
 			}
-
-			auto decode_frame_list(
-				nlohmann::ordered_json &animation_sprite,
-				nlohmann::ordered_json& animation_sprite_map,
-				std::vector<std::string>& animation_sprite_name_list
-			) -> void
+			case 3:
 			{
-				//auto frame_model = std::map<>();
-				return;
+				auto cos = Math::cos(variant_transform[0]);
+				auto sin = Math::sin(variant_transform[0]);
+				transform = {cos, sin, -sin, cos, variant_transform[1], variant_transform[2]};
+				break;
 			}
-
-		public:
-
-			explicit ToFlash(
-				const SexyAnimation& that
-			) : animation(that)
+			case 6:
 			{
-
+				transform = {variant_transform[0], variant_transform[1], variant_transform[2], variant_transform[3], variant_transform[4], variant_transform[5]};
+				break;
 			}
+			default:
+				throw Exception(fmt::format("{}", Language::get("popcap.animation.from_animation.invalid_transform")));
+			}
+		}
+		inline static auto constexpr convert_transform = variant_to_standard;
 
-			~ToFlash(
+		template <typename T, typename E>
+		inline static auto check_element_in_vector(
+			const std::vector<T> & v,
+			const E & e
+		) -> bool
+		 {
+			if (std::find(v.begin(), v.end(), e) != v.end()) {
+				return true;
+			}
+			return false;
+		}
+
+		inline static auto constexpr includes = check_element_in_vector<int, int>;
+	protected:
+		Animation animation;
+
+	protected:
+		template <typename T>
+			requires std::is_integral<T>::value or std::is_floating_point<T>::value
+		auto write_image(
+			XMLDocument *document,
+			std::string_view name,
+			nlohmann::ordered_json &image,
+			T scale) -> void
+		{
+			assert_conditional(image["transform"].get<std::vector<float>>().size() == 6, "transform size must be 6 to write image", "write_image");
+			auto DOMSymbolItem = document->NewElement("DOMSymbolItem");
+			DOMSymbolItem->SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+			DOMSymbolItem->SetAttribute("xmlns", "http://ns.adobe.com/xfl/2008/");
+			DOMSymbolItem->SetAttribute("name", fmt::format("image/{}", name).data());
+			DOMSymbolItem->SetAttribute("symbolType", "graphic");
+			auto timeline = document->NewElement("timeline");
+			auto DOMTimeline = document->NewElement("DOMTimeline");
+			DOMTimeline->SetAttribute("name", name.data());
+			auto layers = document->NewElement("layers");
+			auto DOMLayer = document->NewElement("DOMLayer");
+			auto frames = document->NewElement("frames");
+			auto DOMFrame = document->NewElement("DOMFrame");
+			DOMFrame->SetAttribute("index", 0);
+			auto elements = document->NewElement("elements");
+			auto DOMBitmapInstance = document->NewElement("DOMBitmapInstance");
+			DOMBitmapInstance->SetAttribute("libraryItemName", fmt::format("media/{}", image["name"].get<std::string>()).data());
+			auto matrix = document->NewElement("matrix");
+			auto Matrix = document->NewElement("Matrix");
+			Matrix->SetAttribute("a", to_fixed_6<double>(image["transform"][0].get<double>() * scale).data());
+			Matrix->SetAttribute("b", to_fixed_6<double>(image["transform"][1].get<double>()).data());
+			Matrix->SetAttribute("c", to_fixed_6<double>(image["transform"][2].get<double>()).data());
+			Matrix->SetAttribute("d", to_fixed_6<double>(image["transform"][3].get<double>() * scale).data());
+			Matrix->SetAttribute("tx", to_fixed_6<double>(image["transform"][4].get<double>()).data());
+			Matrix->SetAttribute("ty", to_fixed_6<double>(image["transform"][5].get<double>()).data());
+			matrix->InsertEndChild(Matrix);
+			DOMBitmapInstance->InsertEndChild(matrix);
+			elements->InsertEndChild(DOMBitmapInstance);
+			DOMFrame->InsertEndChild(elements);
+			frames->InsertEndChild(DOMFrame);
+			DOMLayer->InsertEndChild(frames);
+			layers->InsertEndChild(DOMLayer);
+			DOMTimeline->InsertEndChild(layers);
+			timeline->InsertEndChild(DOMTimeline);
+			DOMSymbolItem->InsertEndChild(timeline);
+			document->InsertEndChild(DOMSymbolItem);
+			return;
+		}
+
+		auto decode_frame_list(
+			nlohmann::ordered_json &animation_sprite,
+			nlohmann::ordered_json &animation_sprite_map,
+			std::vector<std::string> &animation_sprite_name_list,
+			std::map<int, std::vector<FrameNode>>& frame_node_list,
+			std::map<std::string, Action>& action_list
+		) -> void
+		{
+			auto sprite_model = std::map<int, Model>{};
+			auto frame_length = animation_sprite["frame"].size();
+			// frame_node_list.emplace(
+			// 	0, std::vector<FrameNode>{}.emplace_back(FrameNode{
+			// 		   0, static_cast<int>(frame_length),
+			// 		   -1, false, -1}));
+			auto main_label = std::string{};
+			for (auto i : Range<int>(frame_length))
+			{
+				auto label = animation_sprite["frame"][i]["label"].get<std::string>();
+				if (!label.empty())
+				{
+					if (main_label.empty())
+					{
+						action_list[main_label].duration = i;
+					}
+					main_label = std::move(label);
+				}
+				if (!(action_list.contains(main_label) && main_label.empty()))
+				{
+					action_list.emplace(main_label, Action{i, 0});
+				}
+				if (i == frame_length - 1 && !main_label.empty())
+				{
+					action_list[main_label].duration = i + 1;
+				}
+				auto frame_remove_length = animation_sprite["frame"][i]["remove"].size();
+				for (auto k : Range<int>(frame_remove_length))
+				{
+					sprite_model[animation_sprite["frame"][i]["remove"][k]].state = 0;
+				}
+				auto frame_additives_length = animation_sprite_map["frame"][i]["append"].size();
+				for (auto k : Range<int>(frame_additives_length))
+				{
+					auto append_index = animation_sprite_map["frame"][i]["append"][k]["index"].get<int>();
+					sprite_model.emplace(
+						append_index,
+						Model{
+							0, -1, animation_sprite_map["frame"][i]["append"][k]["resource"].get<int>(),
+							animation_sprite_map["frame"][i]["append"][k]["sprite"].get<bool>(),
+							i, i});
+					frame_node_list.emplace(
+						(append_index + 1), std::vector<FrameNode>{});
+					if (i > 0)
+					{
+						frame_node_list[(append_index + 1)].emplace_back(FrameNode{
+							0, i, -1, false, -1});
+					}
+				}
+				auto frame_moves_length = animation_sprite_map["frame"][i]["change"].size();
+				for (auto k : Range<int>(frame_moves_length))
+				{
+					auto layer_index = animation_sprite_map["frame"][i]["change"][k]["index"].get<int>();
+					sprite_model[layer_index].state = 1;
+					auto transform = std::array<double, 6>{};
+					convert_transform(transform, animation_sprite_map["frame"][i]["change"][k]["transform"].get<std::vector<double>>());
+					sprite_model[layer_index].transform = transform; 
+					if (!animation_sprite_map["frame"][i]["change"][k]["color"].is_null()) {
+						if (animation_sprite_map["frame"][i]["change"][k]["color"][0].get<double>() != initial_color[0] ||
+							animation_sprite_map["frame"][i]["change"][k]["color"][1].get<double>() != initial_color[1] ||
+							animation_sprite_map["frame"][i]["change"][k]["color"][2].get<double>() != initial_color[2] ||
+							animation_sprite_map["frame"][i]["change"][k]["color"][3].get<double>() != initial_color[3] 
+						) {
+							sprite_model[layer_index].color = animation_sprite_map["frame"][i]["change"][k]["color"].get<std::array<double, 4>>();
+						}
+					}
+				}
+				for (auto &[layer_index, model_layer] : sprite_model) {
+					auto anim_layer_index = layer_index + 1;
+					if (!main_label.empty()) {
+						if (!includes(action_list[main_label].frame_index, anim_layer_index)) {
+							action_list[main_label].frame_index.emplace_back(anim_layer_index);
+						}
+					}
+					if (model_layer.state != -1) {
+						auto frame_node_length = frame_node_list[anim_layer_index].size();
+						if (frame_node_length > 0) {
+							frame_node_list[anim_layer_index][frame_node_length].duration = model_layer.frame_duration;
+						}
+					}
+					if (model_layer.state == 1) {
+						auto first_frame = model_layer.sprite ? (i - model_layer.frame_start) % 
+						animation_sprite_map[animation_sprite_name_list[model_layer.resource]]["frame"].size() : 0;
+						auto frame_node = FrameNode{i, 0, model_layer.resource, model_layer.sprite, static_cast<int>(first_frame)};
+						frame_node.transform = model_layer.transform;
+						frame_node.color = model_layer.color;
+						frame_node_list[anim_layer_index].emplace_back(frame_node);
+						model_layer.state = -1;
+						model_layer.frame_duration = 0;
+					}
+					if (model_layer.state == 0) {
+						sprite_model.erase(layer_index);
+					}
+					++model_layer.frame_duration;
+				}
+			}
+			for (auto &[layer_index, model_layer] : sprite_model) {
+				auto frame_node_length = frame_node_list[(layer_index + 1)].size();
+				frame_node_list[(layer_index + 1)][(frame_node_length - 1)].duration = model_layer.frame_duration;
+				sprite_model.erase(layer_index);
+			}
+			return;
+		}
+
+	public:
+		explicit ToFlash(
+			const SexyAnimation &that) : animation(that)
+		{
+		}
+
+		~ToFlash(
 
 			) = default;
 
-			ToFlash(
-				ToFlash&& that
-			) = delete;
+		ToFlash(
+			ToFlash &&that) = delete;
 
-			auto operator =(
-				ToFlash&& that
-			)->ToFlash & = delete;
+		auto operator=(
+			ToFlash &&that) -> ToFlash & = delete;
 
-
-			auto process(
-				std::string_view destination,
-				int resolution
-			) -> void
+		auto process(
+			std::string_view destination,
+			int resolution) -> void
+		{
+			auto scale_ratio = 1200.0f / static_cast<float>(resolution);
+			auto extra_info = ExtraInfo{
+				.version = animation.version,
+			};
+			for (auto &[key, value] : animation.image.items())
 			{
-				auto scale_ratio = 1200.0f / static_cast<float>(resolution);
-				auto extra_info  = ExtraInfo {
-					.version = animation.version,
+				extra_info.group[key] = nlohmann::ordered_json{
+					{
+						"name",
+						value["name"],
+					},
+					{
+						"size",
+						value["size"],
+					},
 				};
-				for (auto & [key, value] : animation.image.items()) {
-					extra_info.group[key] = nlohmann::ordered_json {
-						{ "name", value["name"],},
-						{ "size", value["size"], },
-					};
-					auto image_document = XMLDocument{};
-					write_image(&image_document, key, value, scale_ratio);
-					FileSystem::write_xml(fmt::format("{}/library/image/{}.xml", destination, key), &image_document);
-				}
-				for (auto& [sprite_name, sprite_value] : animation.sprite.items()) {
-
-				}
-				return;
+				auto image_document = XMLDocument{};
+				write_image(&image_document, key, value, scale_ratio);
+				FileSystem::write_xml(fmt::format("{}/library/image/{}.xml", destination, key), &image_document);
 			}
-
+			for (auto &[sprite_name, sprite_value] : animation.sprite.items())
+			{
+			}
+			return;
+		}
 	};
 }
