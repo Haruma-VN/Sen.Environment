f16eaec90ffde401c58e42dc5a294791c5a6c601
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2024-04-05 17:50:19 +0700

    04.05.2024
    Hide WinShell

--- WinShell/lib/interface/application.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-/// Application default behavior
-
-class Application {
-  //// Application name
-  static const String app_name = 'Sen: WinShell';
-}
